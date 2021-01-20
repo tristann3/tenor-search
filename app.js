5aae371bfb2f21f42fd336abfbbe14825c978d38
Tristan Thompson <tristan15thompson@gmail.com>
2021-01-20 15:41:48 -0800

    added search form

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Routes
-// Homepage route
+
+// example URL "http://localhost:3000/?term=hey"
 app.get('/', (req, res) => {
-  // set the url of the gif
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render('hello-gif', { gifUrl })
-});
+  console.log(req.query) // => "{ term: hey" }
+
+  res.render('home')
+})
 
 // Greeting Route
 app.get('/greetings/:name', (req, res) => {
